9a0de0cdc58eb08b10c805fa948bb7f515265db2
double_h <952385309@qq.com>
2019-08-11 09:11:24 +0800

    允许跨域

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api');
 
 var app = express();
+app.all('*', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');//允许的域名
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');//服务器支持的头信息
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');//允许的方法
+    //res.header("X-Powered-By",' 3.2.1');
+    //res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
